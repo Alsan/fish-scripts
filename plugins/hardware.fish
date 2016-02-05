59acafa3095b2e087db4be0c6d52ae779d7f6a9d
Alsan Wong <awxhuang@fsll.cn>
2016-02-05 10:30:18 +0800

    fix typo

--- plugins/hardware.fish
+++ plugins/hardware.fish
@@ -1,4 +1,4 @@
-function paring --description 'pairing logitech unifying receiver with mouse'
+function pairing --description 'pairing logitech unifying receiver with mouse'
 	sudo unify /dev/hidraw1
 
 	if [ $status -gt 0 ]
