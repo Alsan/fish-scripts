96d21f65d1d7d0eceeaae7e08190a2d44b1cb1f8
Alsan Wong <awxhuang@fsll.cn>
2016-02-05 10:21:37 +0800

    add diff alias

--- plugins/aliases/git.fish
+++ plugins/aliases/git.fish
@@ -7,3 +7,17 @@ alias commit-all 'git commit -am'
 alias flow 'git flow'
 alias add 'git add'
 alias add-all 'git add -A :/'
+
+function __is_inside_git_repo
+	git rev-parse 2> /dev/null
+end
+
+function diff -d 'git diff if under git repo, or normal diff if outside git diff'
+	__is_inside_git_repo
+
+	if [ $status -eq 0 ]
+		git diff $argv
+	else
+		command diff $argv
+	end
+end
