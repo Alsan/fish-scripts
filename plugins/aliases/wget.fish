eb7a1f83582b8b8aabb5baa1b5bcc03d99bcf31a
Alsan Wong <awxhuang@fsll.cn>
2016-02-14 10:53:25 +0800

    remove save dir structure options

--- plugins/aliases/wget.fish
+++ plugins/aliases/wget.fish
@@ -1,4 +1,4 @@
-alias wget 'wget -c -x 16'
+alias wget 'wget -c'
 alias wget-rest 'wget -S -q --header "Accept: application/json" -O - '
 alias curl-rest 'curl -i -H "Accept: application/json"'
 alias aria2c 'aria2c -x 16'
