271e63dc448f1f84eca11b89c51427b6d8a9f3b6
Alsan Wong <awxhuang@fsll.cn>
2016-03-23 16:20:26 +0800

    add touchpad enable/disable script

--- plugins/hardware.fish
+++ plugins/hardware.fish
@@ -5,3 +5,13 @@ function pairing --description 'pairing logitech unifying receiver with mouse'
 		sudo unify /dev/hidraw2
 	end
 end
+
+function touchpad.enable --description 'enable touchpad'
+	xinput set-prop 14 "Device Enabled" 1
+	syndaemon -i 2 -K -d
+end
+
+function touchpad.disable --description 'disable touchpad'
+	killall -9 syndaemon
+	xinput set-prop 14 "Device Enabled" 0
+end
