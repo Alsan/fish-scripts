f7ee3244d212efd42f0c6fa6f46b51230d6ab0b6
Alsan Wong <awxhuang@fsll.cn>
2016-03-23 16:21:55 +0800

    add docker stop script

--- plugins/docker-container.fish
+++ plugins/docker-container.fish
@@ -6,3 +6,13 @@ function docker-container -d 'get docker container id' --argument-names 'name'
 	end
 end
 
+function docker.stop -d 'stop a running docker container' --argument-names name
+	if test -n "$name"
+		set container (docker-container $name)
+		docker stop $container
+		docker rm $container
+	else
+		echo 'syntax: docker.stop [container name]'
+	end
+end
+
