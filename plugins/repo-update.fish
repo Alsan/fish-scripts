b802cd5d732b05f6525289d53ce205c38ccffd66
Alsan Wong <awxhuang@fsll.cn>
2016-03-27 07:30:23 +0800

    rename apt-update-repo to repo-update

--- plugins/repo-update.fish
+++ plugins/repo-update.fish
@@ -1,4 +1,4 @@
-function apt-update-repo --description 'update indivual apt-get repository'
+function repo-update --description 'update indivual apt-get repository'
 	for src in $argv
 		sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/$src" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 	end
