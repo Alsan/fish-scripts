ac70fca5eedabbe563653bc9aa95e5780a662a00
Alsan Wong <awxhuang@fsll.cn>
2016-05-26 10:33:15 +0800

    add aliases to iScript scripts

--- plugins/aliases/cloud_svc.fish
+++ plugins/aliases/cloud_svc.fish
@@ -1,1 +1,13 @@
-alias baidu '/opt/iScript/pan.baidu.com.py'
+set path /opt/iScript
+
+alias baidu $path/pan.baidu.com.py
+alias ed $path/ed2k_search.py
+alias xm $path/xiami.py
+alias bt $path/bt.py
+alias yp $path/yunpan.360.cn.py
+alias bm $path/music.baidu.com.py
+alias nm $path/music.163.com.py
+alias flv $path/flv_cmd.py
+alias tm $path/tumblr.py
+alias pn $path/91porn.py
+
