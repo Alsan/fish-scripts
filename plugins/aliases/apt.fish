dbb8e425052996ceda1da27773573c70d85fd2e0
Alsan Wong <awxhuang@fsll.cn>
2016-06-11 06:47:27 +0800

    mv the function from repo-update.fish to apt.fish

--- plugins/aliases/apt.fish
+++ plugins/aliases/apt.fish
@@ -19,3 +19,8 @@ function apt-remove-old-kernel -d 'remote outdated kernel'
 	sudo update-grub
 end
 
+function apt-repo-update --description 'update indivual apt-get repository'
+	for src in $argv
+		sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/$src" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+	end
+end
--- plugins/repo-update.fish
+++
@@ -1,5 +0,0 @@
-function repo-update --description 'update indivual apt-get repository'
-	for src in $argv
-		sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/$src" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-	end
-end
