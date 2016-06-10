01190e5a7559662491bf58c13b04ec8c0bd81b1d
Alsan Wong <awxhuang@fsll.cn>
2016-06-11 06:48:13 +0800

    remote empty line at the script end

--- plugins/aliases/java.fish
+++ plugins/aliases/java.fish
@@ -2,4 +2,3 @@ alias jar-run 'java -jar'
 alias closure 'jar-run /opt/closure-compiler/compiler.jar'
 alias jd-gui 'jar-run ~/proj/foss/jd-gui/build/libs/jd-gui-1.4.0.jar'
 alias gradle.compile "gradle build --daemon"
-
