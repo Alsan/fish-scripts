d1300292d3236b7fe3519b2fc77ac5f357bd3f05
Alsan Wong <awxhuang@fsll.cn>
2016-12-25 10:25:44 +0800

    fix typo

--- functions/commit-amend-msg.fish
+++ functions/commit-amend-msg.fish
@@ -1,4 +1,3 @@
-function commit-ammend-msg --description 'alias commit-ammend-msg git commit --amend -m'
+function commit-amend-msg --description 'alias commit-ammend-msg git commit --amend -m'
     git commit --amend -m $argv
-
 end
